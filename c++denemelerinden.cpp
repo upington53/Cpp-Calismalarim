e15f699a3b307d58623917676e7b455536fbe15b
Ulaş Pirim <113123120+upington53@users.noreply.github.com>
2024-07-20 15:29:53 +0300

    namespace_kullanimi.cpp

--- c++denemelerinden.cpp
+++ c++denemelerinden.cpp
@@ -6,30 +6,29 @@ using namespace std;
 void constPtr (const int *ptr2,const int *ptr3) {
     std::cout << "pointer b= " << ptr2 << std::endl;
     ptr2++; //burada pointeri degistirmedik. Degerini degistirdik.
-    //*ptr2 = 100; deseydik bu sefer hata verirdi. Cunku const olmasina ragmen pointerý degistirmeye kalktik
+    //*ptr2 = 100; deseydik bu sefer hata verirdi. Cunku const olmasina ragmen pointerÃ½ degistirmeye kalktik
     std::cout << "yeni pointer b= " << ptr2 << std::endl; }
 */
 
-
-struct Calisan { //Gruplaþtýrma ile ileride karakter girdisi yapacaðýz.
+struct Calisan { //Gruplastirma ile ileride karakter girdisi yapacagiz.
     int id; 
     string adi;
     string birimi; 
 };
 
-void isci(Calisan eleman){ //Burada fonksiyon atamasý için koydum. Mesela eleman1 burada bulunuyor.
+void isci(Calisan eleman){ //Burada fonksiyon atamasi iÃ§in koydum. Mesela eleman1 burada bulunuyor.
    cout<< "ID: " << eleman.id << ", Calisan Adi: " << eleman.adi << ", Birimi: " << eleman.birimi << cout;
 }
 
-namespace eslem { //Bunlara isim uzayý deniyor
-	int Plaka =16;
+namespace eslem { //Bunlara isim uzayÃ½ deniyor
+    int Plaka =16;
 }
 
 namespace ulas{
     int Plaka=01;
 }
-int Ulas=53;
-int Eslem=77;
+int Ulas=53; //global bir deÄŸisken
+int Eslem=77; //global bir degisken
 
 int main () 
 {
@@ -42,8 +41,6 @@ int main ()
     std::cout << "a= "<< a << std::endl;
     std::cout << "pointer a= " << ptr1 << std::endl;
     constPtr(&b,&c); */
-    
-
     
     std::cout << "Eslem= "<<Eslem<< ", Ulas= " <<Ulas<<std::endl;
     std::cout << "Eslem= "<<eslem::Plaka<< ", Ulas= " <<ulas::Plaka<<std::endl;
